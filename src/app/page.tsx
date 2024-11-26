10bb6d1aa30f8a7ec9d1eba39a9b48cf6c204884
Emmanuel Tokula <emmanueltokula03@gmail.com>
2024-11-27 00:25:55 +0100

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import VideoIntro from "./video-intro";
 import Feature from "./feature";
 import MobileConvenience from "./mobile-convenience";
 import Testimonials from "./testimonials";
-import Faqs from "./faqs";
 
 export default function Campaign() {
   return (
@@ -18,7 +17,7 @@ export default function Campaign() {
       <Feature />
       <MobileConvenience />
       <Testimonials />
-      {/* <Faqs /> */}
+      
       <Footer />
     </>
   );
