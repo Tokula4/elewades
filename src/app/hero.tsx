2183925116f4fc2f48267e3c3eb9a528b04289b9
Emmanuel Tokula <emmanueltokula03@gmail.com>
2024-11-27 01:58:59 +0100

    updates

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { Button, Typography } from "@material-tailwind/react";
+import { Button,  } from "@material-tailwind/react";
 
 
 function Hero() {
@@ -10,19 +10,19 @@ function Hero() {
       <header className="grid !min-h-[49rem] bg-[#d77dd4] px-8">
         <div className="container mx-auto mt-32 grid h-full w-full grid-cols-1 place-items-center lg:mt-14 lg:grid-cols-2">
           <div className="col-span-1">
-            <Typography variant="h1" color="white" className="mb-4">
+            <div  color="white" className="mb-4 scroll-m-20 text-white pb-2 text-3xl font-semibold tracking-tight first:mt-0">
               Elewade  <br /> Nigeria Limited
-            </Typography>
-            <Typography
-              variant="lead"
+            </div>
+            <div
+              
               className="mb-7 !text-white md:pr-16 xl:pr-28"
             >
               Our app is here to empower you on your quest for knowledge,
               anytime and anywhere.
-            </Typography>
-            <Typography className="mb-4" color="white" variant="h6">
+            </div>
+            <div className="mb-4" color="white" >
               Get the app
-            </Typography>
+            </div>
             <div className="flex flex-col gap-2 md:mb-2 md:w-10/12 md:flex-row">
               <Button
                 size="lg"
@@ -53,17 +53,17 @@ function Hero() {
       </header>
       <div className="mx-8 lg:mx-16 -mt-24 rounded-xl bg-white p-5 md:p-14 shadow-md">
         <div>
-          <Typography variant="h3" color="blue-gray" className="mb-3">
+          <div  color="blue-gray" className="mb-3">
             History of Elewade
-          </Typography>
-          <Typography
-            variant="paragraph"
+          </div>
+          <div
+            
             className="font-normal !text-gray-500 lg:w-5/12"
           >
             Download our app to dive into a vast library of courses, tutorials,
             and study materials on a wide range of subjects - from programming
             and language learning to personal development and beyond
-          </Typography>
+          </div>
         </div>
       </div>
     </div>
